ac9bb0821b7e23f7a5009b4a3ce82d8a8d82fbc6
Spencer Raisanen <r123spencer@hotmail.com>
2023-01-31 11:41:42 -0700

    github action

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -8,7 +8,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://spencerrais.github.io",
-  base: "/blog",
   integrations: [
     tailwind({
       config: {
